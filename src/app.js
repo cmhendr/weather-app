4f7c34d0d33f31da529b5fc5082f437b3c74f409
cmhendr <110801558+cmhendr@users.noreply.github.com>
2022-09-21 22:12:45 -0500

    Change open-source link to open in new tab

--- src/app.js
+++ src/app.js
@@ -21,35 +21,42 @@ let h3 = document.querySelector("h3");
 h3.innerHTML = `${day} ${hours}:${minutes}`;
 
 function showTemperature(response) {
-  let city = response.data.name;
-  let temperature = Math.round(response.data.main.temp);
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${temperature}`;
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `${city}`;
+  document.querySelector("h1").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°F`;
+  document.querySelector("h2").innerHTML = response.data.name;
+}
+
+function searchCity(city) {
+  let apiKey = `eae061c95483dd066657bfc7525418ed`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showTemperature);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#selector-text-input").value;
+  let city = document.querySelector("#search-text-input").value;
   searchCity(city);
 }
 
-function showPosition(position) {
-  let lat = position.coords.lat;
-  let lon = position.coords.lon;
-  let apiKey = `c95d60a1e3adbeb286133f1ebebc2579`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+function searchLocation(position) {
+  let apiKey = `eae061c95483dd066657bfc7525418ed`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(showTemperature);
 }
 
 function getCurrentPosition(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showPosition);
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 let searchForm = document.querySelector("#selector-city-search");
 searchForm.addEventListener("submit", handleSubmit);
 
+let searchButton = document.querySelector("#button-search");
+searchButton.addEventListener("click", handleSubmit);
+
 let currentButton = document.querySelector("#button-current");
 currentButton.addEventListener("click", getCurrentPosition);
+
+searchCity("Chicago");
