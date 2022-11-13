efbd4c020755434a4877961ec1e942d1e751df1f
cmhendr <110801558+cmhendr@users.noreply.github.com>
2022-11-13 00:00:58 -0600

    Added API integration to forecast

--- src/app.js
+++ src/app.js
@@ -38,30 +38,45 @@ function formatDate(timestamp) {
   return `${day}, ${month} ${date}, ${year}, ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = ` <div class="forecast-card-body">
                           <div class="forecast-container"><div class="row">`;
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
                         
                             
                               <div class="col-4">
-                                <div class="forecast-day">${day}</div>
+                                <div class="forecast-day">${formatDay(
+                                  forecastDay.dt
+                                )}</div>
 
                                 <div class="forecast-temp">
                                   <span class="forecast-temperature-max"
-                                    ><strong>66°</strong></span
+                                    ><strong>${Math.round(
+                                      forecastDay.temp.max
+                                    )}°</strong></span
                                   >/<span class="forecast-temperature-min"
-                                    >43°</span
+                                    >${Math.round(forecastDay.temp.min)}°</span
                                   >F
                                 </div>
                                 
                                   <img
-                                    src="http://openweathermap.org/img/wn/10d@2x.png"
+                                    src="http://openweathermap.org/img/wn/${
+                                      forecastDay.weather[0].icon
+                                    }@2x.png"
                                     alt=""
                                     width="42"
                                   />
@@ -69,9 +84,17 @@ function displayForecast() {
                               </div>
                                                          
 `;
+    }
   });
+
   forecastHTML = forecastHTML + `</div></div></div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "3dce9b1c66837262a25b3f448d354a76";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -94,6 +117,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
